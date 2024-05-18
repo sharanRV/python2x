cbc6b11bd6c6a74915a4b74f01a60adb381e4a13
Your Name <you@example.com>
2024-05-18 21:30:13 +0530

    First Commit

---
+++ 18-05-2024/Grade Calculator.py
@@ -0,0 +1,12 @@
+#Grade Calculator
+score=int(input("Enter the Score"))
+if score >= 90 and score <=100:
+    print("Grade A")
+elif score >=80 and score <=89:
+    print("Grade B")
+elif score >=70 and score <=79:
+    print("Grade c")
+elif score >=60 and score <=69:
+    print("Grade D")
+else:
+    print("Grade E")
